40b59e43da1ffeb1d563e1f51cb7348007e773fe
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-13 23:02:12 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,10 @@
 def find_max_value(array)
-  highest = array.sort!
-  
+  count = 0
+while count < array.length do
+  if count > count + 1
+    return count
+  else return count + 1
+end
+count +=
+end
 end
